1987e9dbe05c10a948c9802f941fc7523aa1ae9c
PabloMur <polillomurillo@gmail.com>
2025-09-12 13:01:34 -0300

    Haciendo la segunda consigna

--- src/Entidades/CuentaBancaria.java
+++ src/Entidades/CuentaBancaria.java
@@ -1,8 +1,51 @@
 package Entidades;
 
 public class CuentaBancaria {
+    private static int contador = 0;
     private int id;
     private String nombre;
-    private double salario;
-    
+    private double balance;
+
+    //Constructor
+    public CuentaBancaria(String nombre, double balance) {
+        this.id = contador;
+        contador++;
+        this.nombre = nombre;
+        this.balance = balance;
+    }
+
+    public CuentaBancaria() {
+        this.id = contador;
+        contador++;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getBalance() {
+        return this.balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    //Methods
+    public double Credito(double ingreso) {
+        this.balance += ingreso;
+        return balance;
+    }
+
+    public double Debito(double gasto) {
+        this.balance -= gasto;
+        return balance;
+    }
 }
